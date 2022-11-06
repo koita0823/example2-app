6f78c7c719abefe21288d897ba7e3ba66c550640
koita <koitabashi18@yahoo.co.jp>
2022-11-06 18:15:28 +0900

    リスト

--- resources/views/components/post/myList.blade.php
+++ resources/views/components/post/myList.blade.php
@@ -16,27 +16,33 @@
     <div class="bg-white rounded-md shadow-lg mt-5 mb-5">
         <ul>
             @foreach($posts as $post)
-				<li class="border-b last:border-b-0 border-gray-200 p-4 flex items-start justify-between">
+				<li class="border-b last:border-b-0 border-gray-400 p-4 flex items-start justify-between">
                     <div>
-						<span class="inline-block rounded-full text-gray-600 bg-gray-100 px-2 py-1 text-xs mb-2">
-                                <p>{{ $post->user->name }}</p>
+						<span class="inline-block rounded-full bg-gray-100 px-2 py-1 text-xs mb-2">
+                            <span class="text-blue-500"><p>{{ $post->user->name }}</p></span>
 								{{ $post->created_at->format('Y-m-d H:i') }}/
 								・{{ $post->about->name }}
 								・{{ $post->feeling->name }}
 						</span>   
                         <p class="text-gray-600">{!! nl2br(e($post->content)) !!}</p>
-                            @if (!Auth::user()->is_like($post->id))
-                            <form action="{{ route('post.likes', $post) }}" method="post">
+                            <p>
+                                @if (!Auth::user()->is_like($post->id))
+                                <form action="{{ route('post.likes', $post) }}" method="post">
                                 @csrf
-                                <button onclick="like({{$post->id}})">いいね</button>
-                            </form>
-                            @else
-                            <form action="{{ route('post.unlike', $post) }}" method="post">
+                                    <button onclick="like({{$post->id}})">
+                                        いいね
+                                    </button>
+                                </form>
+                                @else
+                                <form action="{{ route('post.unlike', $post) }}" method="post">
                                 @csrf
                                 @method('DELETE')
-                                <button onclick="unlike({{$post->id}})">いいね解除</button>
-                            </form>
-                            @endif 
+                                    <button onclick="unlike({{$post->id}})">
+                                        いいね解除
+                                    </button>
+                                </form>
+                                @endif 
+                            </p>    
                     </div>
 					<x-post.options :postId="$post->id" :userId="$post->user_id"></x-post.options>
 				</li>					
