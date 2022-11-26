e5f0443b533c7b5785013f116ffeca6f49795b5c
koita <koitabashi18@yahoo.co.jp>
2022-11-26 23:37:34 +0900

    auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-layout title="TOP | NikoLog"> 
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
+            <a href="/post">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,3 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
-    </div>
-</div>
+<h2 class="text-center text-blue-500 text-4xl font-bold mt-8 mb-8">
+    NikoLog
+</h2>
