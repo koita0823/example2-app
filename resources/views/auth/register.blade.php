d7e219d506c80a2261bcd6acc0c1dedeff27db7e
koita <koitabashi18@yahoo.co.jp>
2022-11-26 23:31:47 +0900

    auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-layout title="TOP | NikoLog"> 
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -61,4 +61,4 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-layout>
