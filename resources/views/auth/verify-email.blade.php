f9986565478ddae5f25d91da175988b6ec4e816e
koita <koitabashi18@yahoo.co.jp>
2022-11-26 23:44:06 +0900

    auth

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,9 +1,9 @@
 <x-layout title="TOP | NikoLog"> 
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            <h2 class="text-center text-blue-500 text-4xl font-bold mt-8 mb-8">
+                NikoLog
+            </h2>
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
