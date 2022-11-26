f6c0545e4c5b9fcb6b8980849f1a7e77313e43ea
koita <koitabashi18@yahoo.co.jp>
2022-11-26 23:28:40 +0900

    commit

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-layout title="TOP | NikoLog"> 
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -32,4 +32,4 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-layout>
