824e069de11c759da44f0cac99952937ab3dd18f
koita <koitabashi18@yahoo.co.jp>
2022-11-27 15:33:51 +0900

    ルーディング

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
-});
+}); 
 Route::get('/post', function () {
     return view('post.myPage');
 });  
