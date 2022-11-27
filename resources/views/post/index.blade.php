648766b849a8170b16806bdaaba9b7d56ecd7d3a
koita <koitabashi18@yahoo.co.jp>
2022-11-27 15:10:50 +0900

    blade

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -5,13 +5,5 @@
             NikoLog
         </h2>
         <x-post.myList :posts="$posts" :like_model="$like_model"></x-post.myList>
-        @guest 
-        <div class="flex flex-wrap justify-center">
-            <div class="w-1/2 p-4 flex flex-wrap justify-evenly">
-                <x-element.button-a :href="route('login')">ログイン</x-element.button-a>
-                <x-element.button-a :href="route('register')" theme="secondary">ユーザー登録</x-element.button-a>
-            </div>
-        </div>  
-        @endguest  
     </x-layout.single>                        
 </x-layout>  
