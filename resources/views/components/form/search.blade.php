ad49231b6345d8401e182403d002a9c9bacb6fab
koita <koitabashi18@yahoo.co.jp>
2023-01-10 15:48:57 +0900

    検索機能修正

--- resources/views/components/form/search.blade.php
+++ resources/views/components/form/search.blade.php
@@ -4,7 +4,7 @@
 ])
 <div>
         <form action="{{ route('post.search') }}" method="GET">
-            <input type="text" name="keyword" placeholder="投稿内容を検索" value="{{ $keyword }}">
+            <input type="text" name="keyword" value="{{ $keyword }}">
             <input type="submit" class="border border-transparent
             shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 
             hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2
